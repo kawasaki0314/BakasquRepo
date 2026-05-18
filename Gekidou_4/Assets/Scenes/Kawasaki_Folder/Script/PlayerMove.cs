8fdc68113d769898b539c99bdbdd802ad78c3a4e
kawasaki0314 <fko2647038@stu.o-hara.ac.jp>
2026-05-18 18:20:53 +0900

    20260518_K3

---
+++ Gekidou_4/Assets/Scenes/Kawasaki_Folder/Script/PlayerMove.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float blinkDistance = 5.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+
+        // エラーを防ぐため
+        if (Keyboard.current == null) return;
+
+        // Aを押すと左移動,左を向く
+        if (Keyboard.current.aKey.isPressed)
+        {
+            move.x -= 1;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        // Dを押すと右移動,右を向く
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            move.x += 1;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        // Wを押すと上移動
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move.y += 1;
+        }
+        // Sを押すと下移動
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move.y -= 1;
+        }
+
+        transform.position += (Vector3)(move * speed * Time.deltaTime);
+
+        // 右クリックするとブリンク
+        // 右クリックを検出
+        if(Mouse.current.rightButton.wasPressedThisFrame)
+        {   // 移動キーを押しているとブリンク出来るよ！
+            if (move != Vector2.zero)
+            {
+                transform.position += (Vector3)(move * blinkDistance);
+            }
+        }
+    }
+}
