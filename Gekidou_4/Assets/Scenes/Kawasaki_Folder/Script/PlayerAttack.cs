4df62f3eab0f5a8c9de56f1184d802a763b237fe
kawasaki0314 <fko2647038@stu.o-hara.ac.jp>
2026-05-18 22:51:45 +0900

    20260518_Ka5

---
+++ Gekidou_4/Assets/Scenes/Kawasaki_Folder/Script/PlayerAttack.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using Unity.VisualScripting;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Attack Settings")]
+    public GameObject attackRangeObject; // 攻撃のエフェクト(仮)
+    public float attackDuration = 0.15f;  // 攻撃が表示される時間
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
