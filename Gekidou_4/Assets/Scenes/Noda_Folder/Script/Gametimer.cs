b45343f1299c66c2f92d82d7bba6e3b033930129
soma1795 <fko2647023@stu.o-hara.ac.jp>
2026-05-18 14:59:33 +0900

    20260518_No

---
+++ Gekidou_4/Assets/Scenes/Noda_Folder/Script/Gametimer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Image uiFill;
+    [SerializeField] private TextMeshProUGUI uiText;
+    [SerializeField] private float CountTime;
+    private void Update()
+    {
+        float timer = CountTime - Time.time;
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer % 60);
+        uiText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
